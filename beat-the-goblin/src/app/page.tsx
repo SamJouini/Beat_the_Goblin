17ffc0b54b28f330a4ac55b8fda9d27884947851
SamJouini <samantha.u.h.jouini@gmail.com>
2024-09-11 15:08:25 +0200

    username

--- beat-the-goblin/src/app/page.tsx
+++ beat-the-goblin/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             width={300}
             height={200}
           />
-          <button className={styles.loginButton}> Login </button> {/* ajouter logique login ici */}
+          <button className={styles.loginButton}> Login {/*ajouter logique login ici*/} </button>
         </div>
         <h1 className={styles.title}>Beat the goblin!</h1>
       </header>
@@ -43,6 +43,34 @@ export default function Home() {
               width={800}
               height={600}
             />
+            <div className={styles.userContent}>
+              <div className={styles.frameContainer}>
+                <Image
+                  src="/assets/user_data/frame.png"
+                  alt="Frame"
+                  width={150}
+                  height={150}
+                  className={styles.frame}
+                />
+                <Image
+                  src="/assets/user_data/knight.png"
+                  alt="Knight"
+                  width={130}
+                  height={130}
+                  className={styles.knight}
+                />
+              </div>
+              <div className={styles.bannerContainer}>
+                <Image
+                  src="/assets/user_data/banner.png"
+                  alt="Banner"
+                  width={200}
+                  height={50}
+                  className={styles.banner}
+                />
+                <span className={styles.username}>Username {/*ajouter logique username ici*/}</span>
+              </div>
+            </div>
           </div>
           <div className={styles.paperTodo}>
             <Image
@@ -51,9 +79,27 @@ export default function Home() {
               width={650}
               height={750}
             />
+            <div className={styles.todoContent}>
+              <h2>Grimoire
+                <Image
+                  src="/assets/to-do/feather.png"
+                  alt="Edit"
+                  width={40}
+                  height={40}
+                  className={styles.editButton} //ajouter logique edit todo ici
+                />
+              </h2>
+              <ul className={styles.todoList}>
+                <li>Learn fireball spell</li>
+                <li>Collect dragon scales</li>
+                <li>Brew invisibility potion</li>
+                <li>Practice levitation</li>
+              </ul>
+            </div>
           </div>
         </section>
       </main>
+
 
 
       <footer className={styles.footer}>
