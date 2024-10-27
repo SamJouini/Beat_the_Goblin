58577fe39bceee20f5e5cbe0b4f1b0488b48e3d5
SamJouini <samantha.u.h.jouini@gmail.com>
2024-10-27 11:00:50 +0100

    documentation

--- beat-the-goblin/src/app/components/Footer.tsx
+++ beat-the-goblin/src/app/components/Footer.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
 import styles from "./Footer.module.css";
 
+/**
+ * Footer Component
+ * 
+ * This component renders the footer section of the application, displaying
+ * a cloud image and copyright information.
+ * 
+ * Key Features:
+ * - Displays a cloud image using Next.js Image component for optimized loading.
+ * - Shows copyright text with the current year and author name.
+ * 
+ * Futur implemetation:
+ * - Links to my GitHub (-> other projects).
+ * 
+ */
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
--- beat-the-goblin/src/app/components/Map.tsx
+++ beat-the-goblin/src/app/components/Map.tsx
@@ -2,6 +2,24 @@
 import Image from "next/image";
 import styles from "./Map.module.css";
 
+/**
+ * Map Component
+ * 
+ * This component renders the map section of the application,
+ * currently displaying a title and an image of the map.
+ * 
+ * Key Features:
+ * - Displays a title for the map section.
+ * - Renders an image of the game's map using Next.js Image component.
+ * 
+ * Futur implementations:
+ * - Current idea is to use the map as a planner so the user can manage their tasks up to 6 days ahead.
+ * - While the technical implementation details are still fuzzy, the concept is to associate 
+ *   various map locations with specific weekdays (e.g., Sundays = beach, Mondays = tower, etc.).
+ * - The user would be able to move their avatar (currently the knight) by clicking on the corresponding 
+ * location on the map and to manage the associated day's to-do.
+ */
+
 const Map = () => {
   return (
     <section className={styles.mapSection}>
